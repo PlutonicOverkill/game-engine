7cd8b734953a0fc9e52726eff60b369e9217945d
PlutonicOverkill <dosgamesandstuff@gmail.com>
2017-03-13 20:36:32 +1300

    scrap the old Slot_map

--- src/glare/glare.hpp
+++ src/glare/glare.hpp
@@ -40,81 +40,29 @@ namespace Glare {
 			using type = std::tuple<std::vector<T>...>;
 		};
 
+		template<typename T>
+		class Slot_map;
+
+		template<typename T>
 		struct Slot_index {
-			template<typename T>
-			Slot_map<T>::size_type index;
+			typename Slot_map<T>::size_type index;
 			int counter;
 		};
 
 		/*
-			A container based on std::vector that does not invalidate
-			Handles when elements are added/destroyed,
-			although iterators, pointers and references are invalidated
+			Slot_map
+			Operations:
+			Adding an element
+				
+			Removing an element
+				
 		*/
 		template<typename T>
 		class Slot_map {
 		public:
-			Slot_map() = default;
-			Slot_map(std::initializer_list<T>);
-			Slot_map& operator=(std::initializer_list<T>);
-
-			using value_type = std::vector<T>::value_type;
-			using size_type = std::vector<T>::size_type;
-			using difference_type = std::vector<T>::difference_type;
-			using reference = std::vector<T>::reference;
-			using const_reference = std::vector<T>::const_reference;
-			using pointer = std::vector<T>::pointer;
-			using const_pointer = std::vector<T>::const_pointer;
-			using iterator = std::vector<T>::iterator;
-			using const_iterator = std::vector<T>::const_iterator;
-			using reverse_iterator = std::vector<T>::reverse_iterator;
-			using const_reverse_iterator = std::vector<T>::const_reverse_iterator;
-
-			iterator erase(const_iterator pos);
-			iterator erase(const_iterator first, const_iterator last);
-
-			void push_back(const T& value);
-			void push_back(T&& value);
-
-			template<class... Args>
-			reference emplace_back(Args&&... args);
-			void pop_back();
-
-			void swap(Slot_map& other);
-
-			reference operator[](size_type pos);
-			const_reference operator[](size_type pos) const;
-
-			reference at(size_type pos);
-			const_reference at(size_type pos) const;
-
-			reference back();
-			const_reference back() const;
-
-			reference front();
-			const_reference front() const;
-
-			iterator begin();
-			const_iterator begin() const;
-			const_iterator cbegin() const;
-
-			iterator end();
-			const_iterator end() const;
-			const_iterator cend() const;
-
-			reverse_iterator rbegin();
-			const_reverse_iterator rbegin() const;
-			const_reverse_iterator crbegin() const;
-
-			reverse_iterator rend();
-			const_reverse_iterator rend() const;
-			const_reverse_iterator crend() const;
-
-			bool empty() const;
-
-			size_type size() const;
-
-			void clear();
+			using value_type = T;
+			using size_type = size_t;
+			using difference_type = ptrdiff_t;
 
 			// long-term handle, intended primarily for objects to safetly refer to others
 			// "knows" which container it belongs to
@@ -123,15 +71,68 @@ namespace Glare {
 				Handle() = default;
 				Handle(Slot_map*, int, int);
 			private:
-				Slot_map* ptr;
+				Slot_map* ptr{nullptr};
 				// -1 in either field indicates "not valid"
 				int index{-1};
 				int counter{-1};
 			};
+
+			// wrapper for std::pair<T, int>*
+			template<bool Is_const>
+			class iterator_base {
+			public:
+				iterator_base(std::pair<T, int>*);
+				// default copy, move, destructor are fine
+
+				// should these be const? they don't modify the iterator itself
+				// but they can modify the object pointed to
+				T* operator->() const;
+				T& operator*() const;
+				T& operator[](int) const;
+
+				iterator_base& operator++();
+				iterator_base& operator--();
+				// no postfix operations for you
+
+				iterator_base& operator+=(difference_type);
+				iterator_base& operator-=(difference_type);
+
+				iterator_base operator+(difference_type);
+				iterator_base operator-(difference_type);
+
+				template<typename U>
+				difference_type operator-(iterator_base<U>);
+
+				template<typename U>
+				bool operator==(iterator_base<U>);
+				template<typename U>
+				bool operator!=(iterator_base<U>);
+			private:
+				std::conditional_t<Is_const,
+					const std::pair<T, int>*, std::pair<T, int>*> ptr;
+			};
+
+			using iterator = iterator_base<false>;
+			using const_iterator = iterator_base<true>;
+
+			Slot_map() = default;
+			Slot_map(std::initializer_list<T>);
+			Slot_map& operator=(std::initializer_list<T>);
+
+			// create
+			// buffered_create
+			// delete
+			// buffered_delete
+			// update (does all buffered_[creates|deletes])
+
 		private:
-			std::vector<T> elem;
-			std::vector<Slot_index> elem_index;
+			std::vector<std::pair<T, int>> elem; // .second is for index into elem_index
+			std::vector<Slot_index<T>> elem_index;
 			std::vector<size_type> free_index;
+
+			// creation and deletion is buffered so that it does not invalidate iterators
+			std::vector<Handle> deletion_buffer;
+			std::vector<T> creation_buffer;
 
 			// starts at 0 and increments each time an object is added
 			// used to validate handles
@@ -153,19 +154,19 @@ namespace Glare {
 }
 
 template<typename T>
-inline bool Glare::Utility::Slot_map<T>::empty() const
+bool Glare::Utility::Slot_map<T>::empty() const
 {
 	return elem.empty();
 }
 
 template<typename T>
-inline size_type Glare::Utility::Slot_map<T>::size() const
+Glare::Utility::Slot_map<T>::size_type Glare::Utility::Slot_map<T>::size() const
 {
 	return elem.size();
 }
 
 template<typename T>
-Glare::Utility::Slot_map::Handle::Handle(Slot_map* ptr, int index, int counter)
+Glare::Utility::Slot_map<T>::Handle::Handle(Slot_map<T>* ptr, int index, int counter)
 	:ptr{ptr}, index{index}, counter{counter}
 {}
 
