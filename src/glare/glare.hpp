6dcfd6359b0a4c2c7530ede7114ebc7867af3d7e
PlutonicOverkill <dosgamesandstuff@gmail.com>
2017-03-15 20:40:52 +1300

    Start implementing pointers and iterators

--- src/glare/glare.hpp
+++ src/glare/glare.hpp
@@ -43,79 +43,93 @@ namespace Glare {
 		template<typename T>
 		class Slot_map;
 
-		template<typename T>
-		struct Slot_index {
-			typename Slot_map<T>::size_type index;
-			int counter;
+		// long-term handle, intended primarily for objects to safetly refer to others
+		// "knows" which container it belongs to
+		template<typename T, bool Is_const>
+		class Slot_map_pointer_base {
+			using pointer_type = typename std::conditional_t<Is_const, const Slot_map*, Slot_map*>;
+		public:
+			Slot_map_pointer_base() = default; // doesn't point to a valid object
+			Slot_map_pointer_base(pointer_type, int, int);
+			// default copy, move, destructor are fine
+
+			Slot_map_pointer_base& reset();
+			explicit operator bool() const;
+
+			const T* operator->() const;
+			const T& operator*() const;
+
+			// TODO: static_assert these
+			T* operator->();
+			T& operator*();
+
+			template<bool U>
+			bool operator==(pointer_base<U>);
+			template<bool U>
+			bool operator!=(pointer_base<U>);
+		private:
+			bool is_valid() const;
+
+			pointer_type ptr;
+			// -1 in either field indicates "not valid"
+			Index index{-1};
+			Counter counter{-1};
 		};
 
-		/*
-			Slot_map
-			Operations:
-			Adding an element
-				
-			Removing an element
-				
-		*/
+		// wrapper for std::pair<T, int>*
+		template<typename T, bool Is_const>
+		class Slot_map_iterator_base {
+			using iterator_type = typename std::conditional_t<Is_const, const Slot_map*, Slot_map*>;
+		public:
+			Slot_map_iterator_base(iterator_type, Direct_index);
+			// default copy, move, destructor are fine
+
+			const T* operator->() const;
+			const T& operator*() const;
+			const T& operator[](int) const;
+
+			// TODO: static_assert these
+			T* operator->() const;
+			T& operator*() const;
+			T& operator[](int) const;
+
+			Slot_map_iterator_base& operator++();
+			Slot_map_iterator_base& operator--();
+			// no postfix operations for you
+
+			Slot_map_iterator_base& operator+=(difference_type);
+			Slot_map_iterator_base& operator-=(difference_type);
+
+			template<bool U>
+			difference_type operator-(Slot_map_iterator_base<U>);
+
+			template<bool U>
+			bool operator==(Slot_map_iterator_base<U>);
+			template<bool U>
+			bool operator!=(Slot_map_iterator_base<U>);
+		private:
+			iterator_type ptr{nullptr};
+			Direct_index index{-1};
+		};
+
 		template<typename T>
 		class Slot_map {
+			using Index = int; // index to elem_indirect
+			using Direct_index = int; // index to elem
+			using Counter = int;
 		public:
 			using value_type = T;
 			using size_type = size_t;
 			using difference_type = ptrdiff_t;
+			using pointer = Slot_map_pointer_base<T, false>;
+			using const_pointer = Slot_map_pointer_base<T, true>;
+			using iterator = Slot_map_iterator_base<T, false>;
+			using const_iterator = Slot_map_iterator_base<T, true>;
 
-			// long-term handle, intended primarily for objects to safetly refer to others
-			// "knows" which container it belongs to
-			class Handle {
-			public:
-				Handle() = default;
-				Handle(Slot_map*, int, int);
-			private:
-				Slot_map* ptr{nullptr};
-				// -1 in either field indicates "not valid"
-				int index{-1};
-				int counter{-1};
-			};
-
-			// wrapper for std::pair<T, int>*
-			template<bool Is_const>
-			class iterator_base {
-			public:
-				iterator_base(std::pair<T, int>*);
-				// default copy, move, destructor are fine
-
-				// should these be const? they don't modify the iterator itself
-				// but they can modify the object pointed to
-				T* operator->() const;
-				T& operator*() const;
-				T& operator[](int) const;
-
-				iterator_base& operator++();
-				iterator_base& operator--();
-				// no postfix operations for you
-
-				iterator_base& operator+=(difference_type);
-				iterator_base& operator-=(difference_type);
-
-				iterator_base operator+(difference_type);
-				iterator_base operator-(difference_type);
-
-				template<bool U>
-				difference_type operator-(iterator_base<U>);
-
-				template<bool U>
-				bool operator==(iterator_base<U>);
-				template<bool U>
-				bool operator!=(iterator_base<U>);
-
-				explicit operator bool();
-			private:
-				std::conditional_t<Is_const,
-					const std::pair<T, int>*, std::pair<T, int>*> ptr;
-			};
-
-			using iterator = iterator_base<false>;
-			using const_iterator = iterator_base<true>;
+			template<typename>
+			friend class Slot_map_pointer_base<T>;
+			template<typename>
+			friend class Slot_map_iterator_base<T>;
 
 			Slot_map() = default;
 			Slot_map(std::initializer_list<T>);
@@ -128,18 +142,30 @@ namespace Glare {
 			// update (does all buffered_[creates|deletes])
 
 		private:
-			std::vector<std::pair<T, int>> elem; // .second is for index into elem_index
-			std::vector<Slot_index<T>> elem_index;
+			Slot_map& erase(Direct_index);
+
+			size_type size();
+
+			std::vector<std::pair<T, Index>> elem;
+			std::vector<std::pair<Direct_index, Counter>> elem_indirect;
 			std::vector<size_type> free_index;
 
 			// creation and deletion is buffered so that it does not invalidate iterators
-			std::vector<Handle> deletion_buffer;
-			std::vector<T> creation_buffer;
+			std::vector<pointer> deletion_buffer;
+			std::vector<std::pair<T, Index>> creation_buffer;
 
 			// starts at 0 and increments each time an object is added
 			// used to validate handles
-			int counter{0};
-		};
+			Counter counter{0};
+		}; // Slot_map
+
+		template<typename T>
+		template<bool Is_const>
+		Slot_map<T>::iterator_base<Is_const> operator+(Slot_map<T>::iterator_base<Is_const>, Slot_map<T>::difference_type);
+
+		template<typename T>
+		template<bool Is_const>
+		Slot_map<T>::iterator_base<Is_const> operator-(Slot_map<T>::iterator_base<Is_const>, Slot_map<T>::difference_type);
 }
 
 	// Entity component system
@@ -155,21 +181,120 @@ namespace Glare {
 	}
 }
 
-template<typename T>
-bool Glare::Utility::Slot_map<T>::empty() const
+template<typename T, bool Is_const>
+Glare::Utility::Slot_map_iterator_base<T, Is_const> Glare::Utility::operator+
+(Glare::Utility::Slot_map_iterator_base<T, Is_const> lhs, Glare::Utility::Slot_map<T>::difference_type rhs)
 {
-	return elem.empty();
+	return lhs += rhs;
 }
 
-template<typename T>
-Glare::Utility::Slot_map<T>::size_type Glare::Utility::Slot_map<T>::size() const
+template<typename T, bool Is_const>
+Glare::Utility::Slot_map_iterator_base<T, Is_const> Glare::Utility::operator-
+(Glare::Utility::Slot_map_iterator_base<T, Is_const> lhs, Glare::Utility::Slot_map<T>::difference_type rhs)
 {
-	return elem.size();
+	return lhs -= rhs;
 }
 
-template<typename T>
-Glare::Utility::Slot_map<T>::Handle::Handle(Slot_map<T>* ptr, int index, int counter)
+template<typename T, bool Is_const>
+inline Glare::Utility::Slot_map_iterator_base<T, Is_const>::Slot_map_iterator_base(iterator_type ptr, Direct_index index)
+	:ptr{ptr},
+	index{index}
+{}
+
+template<typename T, bool Is_const>
+Glare::Utility::Slot_map_pointer_base<T, Is_const>::Slot_map_pointer_base(pointer_type ptr, int index, int counter)
 	:ptr{ptr}, index{index}, counter{counter}
 {}
+
+template<typename T, bool Is_const>
+const T* Glare::Utility::Slot_map_pointer_base<T, Is_const>::operator->() const
+{
+	static_assert(!Is_const, "pointer is constant");
+
+	// check that index and counter are in the correct range
+	if (!ptr || index == -1 || counter == -1 || index > ptr->elem_indirect.size())
+		return nullptr;
+
+	auto redirect = ptr->elem_indirect[index];
+	if (redirect.second != counter) // check counters
+		return nullptr;
+	return ptr->elem[redirect].first();
+}
+
+template<typename T, bool Is_const>
+T* Glare::Utility::Slot_map_pointer_base<T, Is_const>::operator->()
+{
+	static_assert(!Is_const, "pointer is constant");
+
+	// check that index and counter are in the correct range
+	if (!ptr || index == -1 || counter == -1 || index > ptr->elem_indirect.size())
+		return nullptr;
+
+	auto redirect = ptr->elem_indirect[index];
+	if (redirect.second != counter) // check counters
+		return nullptr;
+
+	auto redirect = ptr->elem_indirect[index];
+	return ptr->elem[redirect].first();
+}
+
+template<typename T, bool Is_const>
+bool Glare::Utility::Slot_map_pointer_base<T, Is_const>::is_valid() const
+{
+	return operator->(); // will return nullptr if handle is not valid
+}
+
+template<typename T, bool Is_const>
+inline Glare::Utility::Slot_map_pointer_base<T, Is_const>::operator bool() const
+{
+	return is_valid();
+}
+
+/*
+Main functions operate on Direct_indexes
+Iterators call them directly
+Pointer check their counters and then call them
+
+Creation:
+	push_back new elem
+	SUB GETNEW
+	if (!(freelist.empty())
+		get elem_index from freelist
+	else
+		push_back new elem_index
+	END SUB
+	put index of elem
+	set counter
+
+Deletion:
+	CHECK COUNTER
+	elem_index of last elem = elem_index of removal elem
+	elem_index of removal elem = -1
+	swap elems
+	pop
+
+Buffered Creation:
+	push_back new elem to add list
+	CALL GETNEW
+	set new elem's index to newly retrieved index
+	set counter of elem_index to -1
+	return pointer
+
+Buffered Destruction:
+	CHECK COUNTER
+	push_back pointer to destroy list
+
+Clean Creation:
+	move elem from add_list to elem list
+	get its index
+	set that elem_index to point to actual element
+
+Clean Deletion:
+	CALL Deletion
+
+Get
+	Lookup but check counter
+	(check index<size)
+*/
 
 #endif // !GLARE_GLARE_HPP
