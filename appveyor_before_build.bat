11c677d8aab21bb2aabb5a02d1616fd2a19c231a
PlutonicOverkill <PlutonicOverkill@users.noreply.github.com>
2017-03-28 12:30:01 +1300

    Fix Linux builds

--- appveyor_before_build.bat
+++ appveyor_before_build.bat
@@ -3,7 +3,7 @@ CD /D %~dp0
 
 git submodule update --init --recursive
 
-IF NOT EXIST build64 cmake -E make_directory build64
+IF NOT EXIST build64 md build64
 cd build64
-cmake -DCMAKE_INSTALL_PREFIX=bin -G "Visual Studio 15 2017 Win64" ..
+cmake -G "Visual Studio 15 2017 Win64" ..
 cd ..
