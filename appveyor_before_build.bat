70cd727d486ebaff05ad028c7f8cacc117bde7d6
PlutonicOverkill <PlutonicOverkill@users.noreply.github.com>
2017-03-27 20:04:37 +1300

    Update submodules for Appveyor

--- appveyor_before_build.bat
+++ appveyor_before_build.bat
@@ -1,6 +1,8 @@
 @echo off
 CD /D %~dp0
 
+git submodule update --init --recursive
+
 IF NOT EXIST build64 cmake -E make_directory build64
 cd build64
 cmake -DCMAKE_INSTALL_PREFIX=bin -G "Visual Studio 15 2017 Win64" ..
