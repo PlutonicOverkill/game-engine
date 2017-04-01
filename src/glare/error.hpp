f48557baee1d21dd5e9c2a25db0e15411c1b93dd
PlutonicOverkill <PlutonicOverkill@users.noreply.github.com>
2017-04-01 17:35:44 +1300

    Optimize exception constructors (not that it matters)

--- src/glare/error.hpp
+++ src/glare/error.hpp
@@ -2,26 +2,27 @@
 #define GLARE_ERROR_HPP
 
 #include <stdexcept>
+#include <utility>
 
 namespace Glare {
 	namespace Error {
 		class Glare_error : public std::runtime_error {
 		public:
-			Glare_error(std::string s) :std::runtime_error{s}{};
+			Glare_error(std::string s) :std::runtime_error{std::move(s)}{};
 		};
 
 		class Slot_map_pointer_not_valid : public Glare_error {
 		public:
-			Slot_map_pointer_not_valid(std::string s) :Glare_error {s}{};
+			Slot_map_pointer_not_valid(std::string s) :Glare_error {std::move(s)}{};
 		};
 
 		class Slot_map_iterator_out_of_range : public Glare_error {
 		public:
-			Slot_map_iterator_out_of_range(std::string s) :Glare_error {s}{};
+			Slot_map_iterator_out_of_range(std::string s) :Glare_error {std::move(s)}{};
 		};
 
 		class Slot_map_index_out_of_range : public Glare_error {
-			Slot_map_index_out_of_range(std::string s) :Glare_error {s}{};
+			Slot_map_index_out_of_range(std::string s) :Glare_error {std::move(s)}{};
 		};
 	}
 }
