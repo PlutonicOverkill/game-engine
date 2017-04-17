375d3b3a4594f5b6a4e277bd6b2350abd6ac669c
PlutonicOverkill <PlutonicOverkill@users.noreply.github.com>
2017-04-17 16:47:54 +1200

    Start implementing the ECS

--- src/glare/ecs.hpp
+++ src/glare/ecs.hpp
@@ -14,20 +14,20 @@ namespace Glare {
 			template<typename... Ts>
 			struct Typelist {};
 
+			// purposely not defined
 			template<template<typename> class Cont, typename... Args>
 			[[maybe_unused]] std::tuple<Cont<Args>...> typelist_helper(Typelist<Args...>);
 
 			template<template<typename> class Cont, typename T>
 			using Variadic_cont = decltype(typelist_helper<Cont>(std::declval<T>()));
-
-			template<typename T>
-			struct Indexed_element {
-				// TODO
-			};
-
-			template<typename T>
-			using Slot_pointer = typename Slot_map<T>::pointer;
 		}
+
+		// Slot_map<T0>::Index_base<Is_const>
+		// Slot_map<T1>::Index_base<Is_const>
+		// ...
+		// Slot_map<TN>::Index_base<Is_const>
+
+		// std::tuple<Index_base<Slot_map<T>...>> ptr
 
 		// a manager class, how original
 		// T is a list of all the component types usable by Entities
@@ -36,19 +36,208 @@ namespace Glare {
 		public:
 			Entity_manager() = default;
 
-			class Entity {
+			template<bool Is_const>
+			class Entity_base {
 			public:
 
 			private:
 				// safe pointer to each component type
-				std::tuple<Impl::Slot_pointer<T>...> ptr;
-			};
-		private:
-			// Impl::Variadic_cont<Slot_map, Impl::Typelist<Impl::Indexed_element<T>...>> vec;
+				// std::tuple<> ptr;
+			}; // Entity_base
 
-			Slot_map<std::tuple<Impl::Slot_pointer<T>...>> ents;
-		};
+			using Entity = typename Entity_base<false>;
+			using Const_entity = typename Entity_base<true>;
+
+			template<typename... U>
+			class Range {
+			public:
+				explicit Range(Entity_manager*);
+
+				// classception
+				template<bool Is_const>
+				class Iterator_base {
+				public:
+					Iterator_base(Entity_manager*,
+								  typename Slot_map<Entity>::template Iterator_base<Is_const>,
+								  typename Slot_map<Entity>::template Iterator_base<Is_const>);
+
+					Iterator_base& operator++();
+
+					template<bool V>
+					bool operator==(Iterator_base<V>) const;
+					template<bool V>
+					bool operator!=(Iterator_base<V>) const;
+
+					Entity_base<Is_const> operator*();
+				private:
+					Entity_manager* ptr;
+
+					typename Slot_map<Entity>::template Iterator_base<Is_const> iter;
+					typename Slot_map<Entity>::template Iterator_base<Is_const> end;
+				}; // iterator_base
+
+				using iterator = Iterator_base<false>;
+				using const_iterator = Iterator_base<true>;
+
+				iterator begin();
+				const_iterator begin() const;
+				const_iterator cbegin() const;
+
+				iterator end();
+				const_iterator end() const;
+				const_iterator cend() const;
+			private:
+				Entity_manager* ptr;
+			}; // Range
+
+			template<typename... U>
+			Range<U...> filter();
+
+			Entity add();
+
+			template<bool Is_const, typename U, typename... V>
+			bool has_component(Entity_base<Is_const>) const;
+			template<bool Is_const, typename U>
+			bool has_component(Entity_base<Is_const>) const;
+
+			// TODO: variadic overloads
+			template<typename U>
+			const U& component(Const_entity) const;
+			template<typename U>
+			U& component(Entity);
+
+			// TODO: variadic overloads
+			template<typename U>
+			U& make_component(Entity);
+
+			// TODO: variadic overloads
+			template<typename U>
+			U* check_component(Const_entity) const;
+			template<typename U>
+			const U* check_component(Entity);
+		private:
+			// only a nested class to avoid cyclic dependencies
+			template<typename U>
+			struct Indexed_element {
+				U val;
+				typename Slot_map<Entity>::Stable_index index;
+			};
+
+			Impl::Variadic_cont<Slot_map, Impl::Typelist<Indexed_element<T>...>> components;
+
+			Slot_map<Entity> ents;
+		}; // Entity_manager
 	}
+}
+
+/***** IMPLEMENTATION *****/
+
+template<typename... T>
+template<typename... U>
+Glare::Ecs::Entity_manager<T...>::Range<U...>::Range
+(Glare::Ecs::Entity_manager<T...>* ptr)
+	:ptr {ptr}
+{}
+
+template<typename... T>
+template<typename... U>
+Glare::Ecs::Entity_manager<T...>::Range<U...> Glare::Ecs::Entity_manager<T...>::filter()
+{
+	return {this};
+}
+
+template<typename... T>
+template<typename... U>
+template<bool Is_const>
+Glare::Ecs::Entity_manager<T...>::Range<U...>::Iterator_base<Is_const>::Iterator_base
+(Glare::Ecs::Entity_manager<T...>* ptr,
+ typename Slot_map<Entity>::template Iterator_base<Is_const> iter,
+ typename Slot_map<Entity>::template Iterator_base<Is_const> end)
+	:ptr {ptr},
+	iter {iter},
+	end {end}
+{}
+
+template<typename... T>
+template<typename... U>
+template<bool Is_const>
+Glare::Ecs::Entity_manager<T...>::Range<U...>::Iterator_base<Is_const>&
+Glare::Ecs::Entity_manager<T...>::Range<U...>::Iterator_base<Is_const>::operator++()
+{
+	do {
+		++iter;
+	} while (iter != end && !ptr->has_component<U...>(*iter));
+	return *this;
+}
+
+template<typename... T>
+template<typename... U>
+template<bool Is_const>
+template<bool V>
+bool Glare::Ecs::Entity_manager<T...>::Range<U...>::Iterator_base<Is_const>::operator==(
+	Glare::Ecs::Entity_manager<T...>::Range<U...>::Iterator_base<V> rhs) const
+{
+	// probably don't need to compare pointers
+	return iter == rhs.iter;
+}
+
+template<typename... T>
+template<typename... U>
+template<bool Is_const>
+template<bool V>
+bool Glare::Ecs::Entity_manager<T...>::Range<U...>::Iterator_base<Is_const>::operator!=(
+	Glare::Ecs::Entity_manager<T...>::Range<U...>::Iterator_base<V> rhs) const
+{
+	// probably don't need to compare pointers
+	return (*this) != rhs;
+}
+
+template<typename... T>
+template<typename... U>
+typename Glare::Ecs::Entity_manager<T...>::Range<U...>::iterator
+Glare::Ecs::Entity_manager<T...>::Range<U...>::begin()
+{
+	return {ptr, ptr->ents.begin(), ptr->ents.end()};
+}
+
+template<typename... T>
+template<typename... U>
+typename Glare::Ecs::Entity_manager<T...>::Range<U...>::const_iterator
+Glare::Ecs::Entity_manager<T...>::Range<U...>::begin() const
+{
+	return {ptr, ptr->ents.cbegin(), ptr->ents.cend()};
+}
+
+template<typename... T>
+template<typename... U>
+typename Glare::Ecs::Entity_manager<T...>::Range<U...>::const_iterator
+Glare::Ecs::Entity_manager<T...>::Range<U...>::cbegin() const
+{
+	return {ptr, ptr->ents.cbegin(), ptr->ents.cend()};
+}
+
+template<typename... T>
+template<typename... U>
+typename Glare::Ecs::Entity_manager<T...>::Range<U...>::iterator
+Glare::Ecs::Entity_manager<T...>::Range<U...>::end()
+{
+	return {ptr, ptr->ents.end(), ptr->ents.end()};
+}
+
+template<typename... T>
+template<typename... U>
+typename Glare::Ecs::Entity_manager<T...>::Range<U...>::const_iterator
+Glare::Ecs::Entity_manager<T...>::Range<U...>::end() const
+{
+	return {ptr, ptr->ents.cend(), ptr->ents.cend()};
+}
+
+template<typename... T>
+template<typename... U>
+typename Glare::Ecs::Entity_manager<T...>::Range<U...>::const_iterator
+Glare::Ecs::Entity_manager<T...>::Range<U...>::cend() const
+{
+	return {ptr, ptr->ents.cend(), ptr->ents.cend()};
 }
 
 #endif // !GLARE_ECS_HPP
--- src/tests/test_entity_manager.cpp
+++ src/tests/test_entity_manager.cpp
@@ -8,65 +8,72 @@ struct TestA { std::string s; };
 struct TestB { std::string s; };
 struct TestC { std::string s; };
 
-TEST(EntityManager, Constructor)
-{
-	Glare::Ecs::Entity_manager<TestA, TestB, TestC> em;
-
-	auto p1 = em.add();
-	auto p2 = em.add();
-	auto p3 = em.add();
-	auto p4 = em.add();
-
-	em.component<TestA>(p1).s = "1 TestA";
-	em.component<TestC>(p1).s = "1 TestC";
-
-	em.component<TestA>(p2) = {"2 TestA"};
-	em.component<TestB>(p2) = {"2 TestB"};
-
-	em.component<TestB>(p3) = {"3 TestB"};
-	em.component<TestC>(p3) = {"3 TestC"};
-
-	em.component<TestA>(p4).s = "4 TestA";
-	em.component<TestB>(p4).s = "4 TestB";
-	em.component<TestC>(p4).s = "4 TestC";
-
-	/*
-		   p1 p2 p3 p4
-	Test1  x  x     x
-	Test2     x  x  x
-	Test3  x     x  x
-	*/
-
-	for (auto x : em.range()) {
-		TestA a = em.check_component<A>(p1); // throw if doesn't exist
-
-		TestB b = em.assert_component<B>(p2); // crash if doesn't exist (no checking)
-
-		if (em.has_component<B, C>(p3)) {
-			// auto [x, y] = em.component<B, C>(p3); // C++17 only
-
-			std::tuple<B, C> t = em.component<B, C>(p3);
-
-			B b = em.component<B>(p3);
-		}
-
-		{
-			std::tuple<B*, C*> t = em.component_ptr<B, C>(p3);
-			B* b = std::get<B*>(x);
-
-			D* d = en.component_ptr<D>(p3);
-		}
-	}
-
-	for (auto x : em.range<Test1>()) {
-
-	}
-
-	for (auto x : em.range<Test1, Test2>()) {
-
-	}
-
-	for (auto x : em.range<Test1, Test2, Test3>()) {
-
-	}
-}
+//TEST(EntityManager, Constructor)
+//{
+//	Glare::Ecs::Entity_manager<TestA, TestB, TestC> em;
+//
+//	auto p1 = em.add();
+//	auto p2 = em.add();
+//	auto p3 = em.add();
+//	auto p4 = em.add();
+//
+//	em.component<TestA>(p1).s = "1 TestA";
+//	em.component<TestC>(p1).s = "1 TestC";
+//
+//	em.component<TestA>(p2) = {"2 TestA"};
+//	em.component<TestB>(p2) = {"2 TestB"};
+//
+//	em.component<TestB>(p3) = {"3 TestB"};
+//	em.component<TestC>(p3) = {"3 TestC"};
+//
+//	em.component<TestA>(p4).s = "4 TestA";
+//	em.component<TestB>(p4).s = "4 TestB";
+//	em.component<TestC>(p4).s = "4 TestC";
+//
+//	/*
+//		   p1 p2 p3 p4
+//	TestA  x  x     x
+//	TestB     x  x  x
+//	TestC  x     x  x
+//	*/
+//
+//	// component access functions:
+//	// make_component(): create it if it doesn't exist
+//	// component(): throw if it doesn't exist
+//	// check_component(): return nullptr if it doesn't exist
+//
+//	// has_component(): return true if component exists
+//
+//	for (auto x : em.filter()) {
+//		TestA a = em.component<TestA>(p1); // throw if doesn't exist
+//
+//		if (em.has_component<TestB, TestC>(p3)) {
+//			// auto [x, y] = em.component<B, C>(p3); // C++17 only
+//
+//			// TODO
+//			// std::tuple<TestB, TestC> t = em.component<TestB, TestC>(p3);
+//
+//			TestB b = em.component<TestB>(p3);
+//		}
+//
+//		{
+//			// TODO
+//			// std::tuple<TestB*, TestC*> t = em.check_component<TestB, TestC>(p3);
+//			// TestB* b = std::get<TestB*>(x);
+//
+//			TestC* c = em.check_component<TestC>(p3);
+//		}
+//	}
+//
+//	for (auto x : em.filter<TestA>()) {
+//
+//	}
+//
+//	for (auto x : em.filter<TestA, TestB>()) {
+//
+//	}
+//
+//	for (auto x : em.filter<TestA, TestB, TestC>()) {
+//
+//	}
+//}
